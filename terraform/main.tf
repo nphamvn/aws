112c4e315afca7ad7df7ca22816a49578d05b384
Nam Pham <pvnam95@hotmail.com>
2025-10-16 18:36:46 +0900

    fix: add missing egress rule to security group for all outbound traffic

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,6 +61,13 @@ resource "aws_security_group" "allow_icmp" {
     to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 ##################################################
