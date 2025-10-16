2cebb430efff7b7cc77af4a26e23328c7cc8c904
Nam Pham <pvnam95@hotmail.com>
2025-10-16 17:39:55 +0900

    feat: add SSH key pair and update security group for inbound SSH access

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,14 +54,26 @@ resource "aws_security_group" "allow_icmp" {
     to_port     = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 ##################################################
 # EC2
 ##################################################
+resource "aws_key_pair" "ssh" {
+  public_key = file("id.pub")
+}
+
 resource "aws_instance" "public" {
   ami                         = "ami-070e0d4707168fc07"
   instance_type               = "t2.nano"
+  key_name                    = aws_key_pair.ssh.key_name
   subnet_id                   = aws_subnet.public.id
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.allow_icmp.id]
