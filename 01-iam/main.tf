c2405314f0a13be6b783b4b7aa053aabcc06a584
Nam Pham <pvnam95@hotmail.com>
2025-10-17 19:04:31 +0900

    feat: add IAM policy attachment for S3 read-only access and update checks for S3 bucket listing

--- 01-iam/main.tf
+++ 01-iam/main.tf
@@ -18,6 +18,12 @@ resource "aws_iam_access_key" "ak" {
   user = aws_iam_user.user.name
 }
 
+resource "aws_iam_policy_attachment" "att" {
+  name = "s3-read-only"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  users = [ aws_iam_user.user.name ]
+}
+
 check "ping" {
   data "aws_caller_identity" "id" {
     provider = aws.test_user
@@ -26,6 +32,17 @@ check "ping" {
   assert {
     condition     = data.aws_caller_identity.id.account_id != ""
     error_message = "Unable to connect to AWS with new IAM user credentials"
+  }
+}
+
+check "list-s3-buckets" {
+  data "aws_s3_bucket" "buckets" {
+    provider = aws.test_user
+  }
+
+  assert {
+    condition     = data.aws_s3_buckets.buckets.ids != null
+    error_message = "IAM user unable to list S3 buckets"
   }
 }
 
