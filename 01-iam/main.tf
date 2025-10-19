8d9a65ff4a6214179b0f83935ccd8b934297ca93
Nam Pham <pvnam95@hotmail.com>
2025-10-19 13:13:12 +0900

    feat: update Terraform configuration to include S3 backend and adjust IAM user checks

--- 01-iam/main.tf
+++ 01-iam/main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/aws"
     }
   }
+  backend "s3" {
+    bucket = "075313985331-terraform"
+    key    = "01-iam/terraform.tfstate"
+  }
 }
 
 provider "aws" {
@@ -18,36 +22,19 @@ resource "aws_iam_access_key" "ak" {
   user = aws_iam_user.user.name
 }
 
-resource "aws_iam_policy_attachment" "att" {
-  name = "s3-read-only"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-  users = [ aws_iam_user.user.name ]
+provider "aws" {
+  alias      = "test_user"
+  access_key = aws_iam_access_key.ak.id
+  secret_key = aws_iam_access_key.ak.secret
 }
 
-check "ping" {
+check "ping" { // maybe failed on first apply because IAM user is not yet fully propagated
   data "aws_caller_identity" "id" {
     provider = aws.test_user
   }
 
   assert {
-    condition     = data.aws_caller_identity.id.account_id != ""
+    condition     = data.aws_caller_identity.id.user_id != ""
     error_message = "Unable to connect to AWS with new IAM user credentials"
   }
-}
-
-check "list-s3-buckets" {
-  data "aws_s3_bucket" "buckets" {
-    provider = aws.test_user
-  }
-
-  assert {
-    condition     = data.aws_s3_buckets.buckets.ids != null
-    error_message = "IAM user unable to list S3 buckets"
-  }
-}
-
-provider "aws" {
-  alias      = "test_user"
-  access_key = aws_iam_access_key.ak.id
-  secret_key = aws_iam_access_key.ak.secret
 }
