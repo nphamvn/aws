4fee289ea212c5dbefc3b787afaa5b23f12566ba
Nam Pham <pvnam95@hotmail.com>
2025-10-20 22:38:54 +0900

    fix: improve comments for EKS Node Group IAM role dependencies

--- 99-eks/node_group.tf
+++ 99-eks/node_group.tf
@@ -10,7 +10,10 @@ resource "aws_eks_node_group" "node_group" {
 
   subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
 
-  depends_on = [aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy
   ]
